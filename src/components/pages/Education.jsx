1d32c753b9f4003bdb02d1823a3f7e00f324130d
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-02-24 16:01:54 -0500

    Modified the CSS for Education

--- src/components/pages/Education.jsx
+++ src/components/pages/Education.jsx
@@ -6,13 +6,15 @@ function Education() {
                <div>
                     <h3>Education</h3>
                     <div>
-                         <p>Full Stack Coding Bootcamp from UC Berkeley (pursuing)</p>
-                         <p>Master of Science, Information Technology, University of the Cumberlands, U.S. (pursuing)</p>
-                         <p>Bachelor of Engineering, Computer Science, Anna University, India. 2002 - 2006.</p>
-                         <p>AWS Certified Cloud Practitioner</p>
-                         <p>OutSystems Associate Reactive Developer O11 – Certificate ID # 0000000097059828</p>
-                         <p>Certified IT Service Management Foundation™ - Certificate ID # - 19344</p>
-                         <p>ITIL® Foundation Certificate in IT Service Management - Certification # GR671139701VS</p>
+                         <ul>
+                              <li><b>Full Stack Coding Bootcamp at UC Berkeley </b>(pursuing)</li>
+                              <li><b>Master of Science,</b> Information Technology, University of the Cumberlands, U.S. (pursuing)</li>
+                              <li><b>AWS Certified Cloud Practitioner</b></li>
+                              <li><b>Bachelor of Engineering,</b> Computer Science, Anna University, India. 2002 - 2006.</li>
+                              <li>OutSystems Associate Reactive Developer O11 – Certificate ID # 0000000097059828</li>
+                              <li>Certified IT Service Management Foundation™ - Certificate ID # - 19344</li>
+                              <li>ITIL® Foundation Certificate in IT Service Management - Certification # GR671139701VS</li>
+                         </ul>
                     </div>
                </div>
           </section>
