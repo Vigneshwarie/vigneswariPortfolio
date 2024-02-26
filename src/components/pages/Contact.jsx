79447af7a9fab4acadf16f1efbb1c33c6bb94548
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-02-26 15:32:39 -0500

    Fixed contact page alignment and CSS

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -11,7 +11,7 @@ function Contact() {
                     <h3>Contact Me</h3>
                     <div>
                          <p>Hello, and thank you for visiting my portfolio! I'm actively seeking opportunities in Staff Software Engineer roles, and I'm excited about the possibility of joining a dynamic team where I can contribute my expertise and passion for software development.</p>
-                         <p>Whether you're a hiring manager, recruiter, or fellow developer interested in discussing potential collaborations or job opportunities, I'd love to hear from you. Feel free to contact me using the contact form below or via email at vigneswari.sambandam@gmail.com. I prioritize responding promptly to all inquiries, so you can expect to hear back from me soon.
+                         <p>Whether you're a hiring manager, recruiter, or fellow developer interested in discussing potential collaborations or job opportunities, I'd love to hear from you. Feel free to contact me using the contact form below or via email <a href="mailto:vigneswari.sambandam@gmail.com"><b>here</b></a>. I prioritize responding promptly to all inquiries, so you can expect to hear back from me soon.
                          </p>
                          <p>Thank you for considering me for your team. I'm eager to discuss how my skills and experience align with your organization's needs and how I can help drive its success.</p>
                          <p>I am looking forward to connecting with you!</p>
@@ -19,9 +19,9 @@ function Contact() {
                </div>
                <section>
                     <div>
-                         <Form>
+                         <Form className="frm">
                               <Form.Group as={Row} className="mb-3" controlId="formHorizontalName">
-                                   <Form.Label column sm={2}>
+                                   <Form.Label column sm={1} className="lbl">
                                         Name
                                    </Form.Label>
                                    <Col sm={4}>
@@ -29,7 +29,7 @@ function Contact() {
                                    </Col>
                               </Form.Group>
                               <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
-                                   <Form.Label column sm={2}>
+                                   <Form.Label column sm={1} className="lbl">
                                         Email
                                    </Form.Label>
                                    <Col sm={4}>
@@ -37,7 +37,7 @@ function Contact() {
                                    </Col>
                               </Form.Group>
                               <Form.Group as={Row} className="mb-3" controlId="formHorizontalMessage">
-                                   <Form.Label column sm={2}>
+                                   <Form.Label column sm={1} className="lbl">
                                         Message
                                    </Form.Label>
                                    <Col sm={4}>
@@ -45,7 +45,7 @@ function Contact() {
                                    </Col>
                               </Form.Group>
                               <Form.Group as={Row} className="mb-3">
-                                   <Col>
+                                   <Col sm={3}>
                                         <Button type="button" className="btn">Submit</Button>
                                    </Col>
                               </Form.Group>
