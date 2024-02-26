8cafe536b1133a827511e74c1582561a30f8c943
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-02-26 03:22:30 -0500

    Modified CSS overall and added profile image in about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import Row from 'react-bootstrap/Row';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
 import "../../assets/styles/About.css";
+import munchmonitor from "../../assets/projects/mmlogo1.png";
 
 function About() {
      return (
@@ -7,10 +11,15 @@ function About() {
                <div>
                     <h3>About Me</h3>
                     <div>
-                         <p>
-                              This is Vigneswari Sambandam aka Viggy. I am currently doing my masters in Information Technology. I'm an accomplished IT professional with 14 years of experience developing and architecting IT applications across industries with a proven track record of delivering value to Healthcare, Telecom, and Service Industries businesses. I wanted to upskill myself and embrace the changes and happenings in recent years in IT. So, I took a break and came to the US for a master's, and now I'm also doing the Berkeley Coding Bootcamp for additional skills. Apart from work and studies, I love watching movies, listening to music, and sometimes reading books. I empathize with people and hate all kinds of discrimination against people in all forms.
-                         </p>
-                    </div>   
+                         <Row>
+                              <Col xs={4}>
+                                   <Image src={munchmonitor} thumbnail className='imgAboutMe' />
+                              </Col>
+                              <Col className="aboutme" xs={8}>
+                                   This is Vigneswari Sambandam aka Viggy. I am currently doing my masters in Information Technology. I'm an accomplished IT professional with 14 years of experience developing and architecting IT applications across industries with a proven track record of delivering value to Healthcare, Telecom, and Service Industries businesses. I wanted to upskill myself and embrace the changes and happenings in recent years in IT. So, I took a break and came to the US for a master's, and now I'm also doing the Berkeley Coding Bootcamp for additional skills. Apart from work and studies, I love watching movies, listening to music, and sometimes reading books. I empathize with people and hate all kinds of discrimination against people in all forms.
+                              </Col>
+                         </Row>
+                    </div>  
                </div>
           </section>
      );
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,5 +1,3 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
