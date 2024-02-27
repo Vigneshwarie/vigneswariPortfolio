363a0f65b7c8d9464e1ba44a7f0d598f41796412
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-02-26 19:28:14 -0500

    Changed profile picture to avatar in about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -3,7 +3,7 @@ import Row from 'react-bootstrap/Row';
 import Image from 'react-bootstrap/Image';
 import Col from 'react-bootstrap/Col';
 import "../../assets/styles/About.css";
-import munchmonitor from "../../assets/projects/mmlogo1.png";
+import womenAvatar from "../../assets/images/womenAvatar.jpg";
 
 function About() {
      return (
@@ -13,10 +13,10 @@ function About() {
                     <div>
                          <Row>
                               <Col xs={4}>
-                                   <Image src={munchmonitor} thumbnail className='imgAboutMe' />
+                                   <Image src={womenAvatar} thumbnail className='imgAboutMe' />
                               </Col>
                               <Col className="aboutme" xs={8}>
-                                   This is Vigneswari Sambandam aka Viggy. I am currently doing my masters in Information Technology. I'm an accomplished IT professional with 14 years of experience developing and architecting IT applications across industries with a proven track record of delivering value to Healthcare, Telecom, and Service Industries businesses. I wanted to upskill myself and embrace the changes and happenings in recent years in IT. So, I took a break and came to the US for a master's, and now I'm also doing the Berkeley Coding Bootcamp for additional skills. Apart from work and studies, I love watching movies, listening to music, and sometimes reading books. I empathize with people and hate all kinds of discrimination against people in all forms.
+                                   This is Vigneswari Sambandam aka Viggy. I am currently doing my masters in Information Technology. I'm an accomplished IT professional with 14 years of experience developing and architecting IT applications across industries with a proven track record of delivering value to Healthcare, Telecom, and Service Industries businesses. I wanted to upskill myself and embrace the changes and happenings in recent years in IT. So, I took a break and came to the US for a master's, and now I'm also doing the Berkeley Coding Bootcamp for additional skills. Apart from work and studies, I love watching movies, listening to music, and sometimes reading books. I empathize with people and hate all kinds of discrimination against individuals.
                               </Col>
                          </Row>
                     </div>  
